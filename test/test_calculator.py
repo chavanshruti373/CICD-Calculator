25e5c8e1fce396fc8b89796926fdf5551ac8cf50
chavanshruti373 <chavanshruti373@gmail.com>
2026-05-13 15:04:08 +0530

    first commit

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,15 +1,15 @@
-from app.calculator import add, subtract, multiply, divide  
+from app.calculator import add, subtraction, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
-    
 
-def test_subtract():
-    assert subtract(5, 2) == 3
+
+def subtraction():
+    assert subtraction(5, 2) == 3
 
 def test_multiply():
     assert multiply(2, 4) == 8
-    
+
 
 def test_divide():
     assert divide(10, 2) == 5
